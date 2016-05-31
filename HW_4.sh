68ffda68ab602ae7d54758f31e5f61aa3d09a127
LIXiao <xli111@ucsc.edu>
2016-05-31 00:09:29 -0700

    add input

---
+++ HW_4.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+javac -d . ./evaluator/ast/*.java
+javac -cp . -d . ./evaluator/cek/*.java
+javac -cp . -d . ./evaluator/*.java
+javac -cp . -d . ./evaluator/test/*.java
+javac -cp . -d . ./front_end/*.java
+javac -cp . -d . ./front_end/test/*.java
+
+java ParserRunner < test_input.txt > test_output.txt
